a9efea5afed9a8010c5e59b0ada487ebfb230b21
Bimal Paul <bimalbpaul@outlook.com>
2019-03-28 15:53:06 -0700

    using react-redux atw

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import InvitePage from './components/pages/invite';
+import { Provider } from 'react-redux';
+import store from './state/store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <InvitePage />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <InvitePage />
+        </div>
+      </Provider>
     );
   }
 }
