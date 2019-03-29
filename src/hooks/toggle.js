ba8210970cd4bf583f65aa0484ccb211a969c6eb
Bimal Paul <bimalbpaul@outlook.com>
2019-03-29 15:00:29 -0700

    semicolon dih

--- src/hooks/toggle.js
+++ src/hooks/toggle.js
@@ -3,6 +3,6 @@ import {useState} from 'react';
 export default initialValue => {
     const [val, setVal] = useState(initialValue);
     const eventHandler = () => setVal(!val);
-    return [ val, eventHandler]
+    return [val, eventHandler];
 }
 
